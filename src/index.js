96c59ede23da5a7953d1ea221a373e2247422577
Bkantwi <bernardantwi2010@gmail.com>
2025-05-08 09:38:15 +0000

    structure

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 
 import App from './App';
 
-const rootElement = document.getElementById('root');
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(<App />);
+ReactDOM.render(<App />, document.getElementById('root'));
